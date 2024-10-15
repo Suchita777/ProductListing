c9532cc8a745f69f2b883d310ef3b1c16ada65c1
Suchita Mishra <suchitamishra318@gmail.com>
2024-10-15 21:26:14 +0000

    Product Listing Page components added

--- main.js
+++ main.js
@@ -1,26 +1,70 @@
 import './style.css';
 
 document.querySelector('#app').innerHTML = `
-  <header>
-      <nav class="navbar">
-          <div class="logo">
-              <a href="#">
-                  <img src="/icons/Venia.png" alt="logo" />
-              </a>
-          </div>
-          <ul class="nav-links">
-              <li><a href="#">Home</a></li>
-              <li><a href="#">Women</a></li>
-              <li><a href="#">Men</a></li>
-              <li><a href="#">Smart Gear</a></li>
-              <li><a href="#">Accessories</a></li>
-          </ul>
-          <div class="cart">
-              <a href="#">
-                  <img src="/icons/cart.svg" alt="Cart Icon" />
-                  <span class="cart-count">10</span>
-              </a>
-          </div>
-      </nav>
-  </header>
+  
 `;
+
+
+const productList = document.getElementById("product-list");
+
+let allProducts = [];
+let filteredProducts = [];
+let wishlist = [];
+let productsToDisplay = 10;
+let currentProducts = 0;
+let loading = false;
+
+
+// Fetch products from the API
+async function fetchProducts() {
+    try {
+    //   loading = true;
+    //   loadingSpinner.style.display = "block";
+      const response = await fetch("https://fakestoreapi.com/products");
+      const products = await response.json();
+      allProducts = products;
+      filteredProducts = products;
+      displayProducts();
+    } catch (error) {
+    //   errorMessage.textContent =
+    //     "Failed to load products. Please try again later.";
+    } finally {
+    //   loading = false;
+    //   loadingSpinner.style.display = "none";
+    }
+  }
+  
+
+  // Display products on the page
+function displayProducts() {
+    productList.innerHTML = "";
+    const productsToShow = filteredProducts.slice(
+      0,
+      currentProducts + productsToDisplay
+    );
+  
+    productsToShow.forEach((product) => {
+      const productCard = document.createElement("div");
+      productCard.classList.add("product");
+      productCard.innerHTML = `
+        <img src="${product.image}" alt="${product.title}">
+        <h3>${product.title}</h3>
+        <p class="price"><strong>$${product.price}</strong></p>
+      `;
+      productList.appendChild(productCard);
+    });
+  
+    currentProducts = productsToShow.length;
+  
+    // Hide the load more button if all products are displayed
+    if (currentProducts >= filteredProducts.length) {
+      loadMoreButton.style.display = "none";
+    } else {
+      loadMoreButton.style.display = "block";
+    }
+  
+    attachWishlistListeners();
+}
+  
+// Initial load
+fetchProducts();
