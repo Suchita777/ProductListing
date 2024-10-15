d94425a12849814f1be63618a0c9c574d30b0992
Suchita Mishra <suchitamishra318@gmail.com>
2024-10-15 10:48:44 +0000

    Added header

--- main.js
+++ main.js
@@ -1,7 +1,26 @@
 import './style.css';
 
 document.querySelector('#app').innerHTML = `
-  <div>
-      Hello world!
-  </div>
+  <header>
+      <nav class="navbar">
+          <div class="logo">
+              <a href="#">
+                  <img src="/icons/Venia.png" alt="logo" />
+              </a>
+          </div>
+          <ul class="nav-links">
+              <li><a href="#">Home</a></li>
+              <li><a href="#">Women</a></li>
+              <li><a href="#">Men</a></li>
+              <li><a href="#">Smart Gear</a></li>
+              <li><a href="#">Accessories</a></li>
+          </ul>
+          <div class="cart">
+              <a href="#">
+                  <img src="/icons/cart.svg" alt="Cart Icon" />
+                  <span class="cart-count">10</span>
+              </a>
+          </div>
+      </nav>
+  </header>
 `;
